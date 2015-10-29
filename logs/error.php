6bb101a65c1bf32a1804dd35a3b51c3953cf76a4
NallelyFlores89 <anjudark89@gmail.com>
2015-10-29 15:52:35 -0600

    basvec

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-10-04 18:00:10 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2014-10-04T18:00:10+00:00	INFO 127.0.0.1	joomlafailure	Empty password not allowed
+2014-10-04T18:00:23+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
